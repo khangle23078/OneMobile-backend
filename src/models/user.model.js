fa2e9b18d1f8f2be30b179d7f6115850f28b975c
DESKTOP-D314UU8\khangle2307 <khangle2307@gmail.com>
2023-06-26 23:19:41 +0700

    change user model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -10,10 +10,6 @@ const userSchema = new Schema(
     password: {
       type: String,
       require: true,
-    },
-    userName: {
-      type: String,
-      required: true
     }
   },
   { timestamps: true }
