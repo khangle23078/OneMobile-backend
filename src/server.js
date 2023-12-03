672ea58a0811fcfd1f5c7c106bc27c19f49de884
DESKTOP-D314UU8\khangle2307 <khangle2307@gmail.com>
2023-12-03 17:55:18 +0700

    set payload request middleware

--- src/server.js
+++ src/server.js
@@ -16,7 +16,8 @@ const app = express();
 const PORT = process.env.PORT || 5001;
 connectDb();
 
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true }))
 app.use(helmet());
 app.use(cors())
 app.use(morgan("combined"));
