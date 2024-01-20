7e2bfead54d44f19524efd8ae76f43a525a281cc
DESKTOP-D314UU8\khangle2307 <khangle2307@gmail.com>
2024-01-20 17:55:28 +0700

    feat: add pagination

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -11,10 +11,19 @@ import { Category } from './../models/category.model.js'
 
 export const getProducts = async (req, res) => {
   try {
-    const products = await getAll();
+
+    const page = parseInt(req.query.page) || 1
+    const limit = parseInt(req.query.limit) || 10
+    const total_product = await getAll();
+    const products = await getAll(limit, page);
     res.status(200).json({
       status: 200,
       data: products,
+      pagination: {
+        page: page,
+        limit: limit,
+        total_item: total_product.length
+      },
       error: false,
     });
   } catch (error) {
